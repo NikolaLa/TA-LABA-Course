756aa24f5539ef8b18febd2871713925dc8c5982
Nikola L <nikollauer@gmail.com>
2024-10-30 15:21:33 +0100

    Insertionsort update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static final void HelloWorld(String[] args) {
+    public static void main(String[] args) {
         System.out.print("Hello"+args[0]);
     }
 }
--- InsertionSort.java
+++ InsertionSort.java
@@ -1,30 +1,34 @@
 public class InsertionSort {
     public static void main(String[] args) {
-        //Buble sort
+        //Insertion Sort
         //initial variable
         int[] array = {4, 15, 23, 3, 1, 2, 7, 19, 2, 6}; // for sorting
-        int temp = array[0];
-        int[] sorted = {0};
+        int temp;
+        int[] sorted = new int[array.length];
         boolean once = true;
         //comparing and sorting
 
-        for (int x = 1; x < array.length ; x++) { // loop one minus the size of array
-
+        for (int x = 1; x < array.length -1 ; x++) { // loop one minus the size of array
+            temp = array[0];
             for (int y = 0; y < array.length-1; y++) { //searching for the smallest number in array
-                if (temp > array[y + 1] && once == true) { // searching for first smallest number only once
-                    temp = array[y+1];
-                    if (array.length - 1 == y) {
-                        once = false;
-                    }
-                }
-                if (temp > array[y + 1] && sorted[x] < temp) { // if smaller than next comparison
-                    temp = array[y];
-                }
-                if (y == array.length - 1) { // if larger swap
-                    sorted[x] = temp;
 
+                if (temp > array[y + 1]) { // if smaller than next comparison
+                    temp = array[y + 1];
                 }
             }
+            int[] newArray = new int[array.length-1];
+
+            for(int z =0, y=0; z < array.length;z++) { //deleting the smallest number from array
+
+                if( array[z]!=temp)
+                {
+                    newArray[y] = array[z];
+                    y++;
+                }
+
+            }
+            array = newArray;
+            sorted[x-1] = temp;
         }
         System.out.println("the sorted numbers");
         for (int x = 0; x < sorted.length; x++) {
