f555c7b257d9c8c7bfd9484c7b7795dd4368fa46
Nikola L <nikollauer@gmail.com>
2024-10-30 15:48:11 +0100

    Insertionsort update

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,35 +4,20 @@ public class InsertionSort {
         //initial variable
         int[] array = {4, 15, 23, 3, 1, 2, 7, 19, 2, 6}; // for sorting
         int temp;
-        int[] sorted = new int[array.length];
-        boolean once = true;
         //comparing and sorting
 
-        for (int x = 1; x < array.length -1 ; x++) { // loop one minus the size of array
-            temp = array[0];
-            for (int y = 0; y < array.length-1; y++) { //searching for the smallest number in array
+        for (int x = 1; x < array.length ; x++) { // loop one minus the size of array
+            temp = array[x-1];
+           for(int y = x ; y<array.length ;y++) {
+               if(temp > array[y]){
+                   temp = array[y];
+                   array[y] =array[x-1];
+                   array[x-1] = temp;
+               }
 
-                if (temp > array[y + 1]) { // if smaller than next comparison
-                    temp = array[y + 1];
-                }
-            }
-            int[] newArray = new int[array.length-1];
-
-            for(int z =0, y=0; z < array.length;z++) { //deleting the smallest number from array
-
-                if( array[z]!=temp)
-                {
-                    newArray[y] = array[z];
-                    y++;
-                }
-
-            }
-            array = newArray;
-            sorted[x-1] = temp;
-        }
-        System.out.println("the sorted numbers");
-        for (int x = 0; x < sorted.length; x++) {
-            System.out.println(sorted[x]);
-        }
+           }
     }
+
+        System.out.print(array);
+}
 }
