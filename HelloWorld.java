9f14934ead42609903a42a86de7735ff5f6b4245
Nikola L <nikollauer@gmail.com>
2024-10-31 12:07:31 +0100

    HelloWorld update

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@
  */
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello"+args[0]);
+    public static void main(String args[]) {
+        System.out.println("Hello " + args[0]);
     }
 }
