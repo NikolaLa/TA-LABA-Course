17bae6b6e8bef3b330ed6fd1979c5f9dd5c667b0
Nikola L <nikollauer@gmail.com>
2024-10-31 14:57:02 +0100

    HelloWorld update

--- InsertionSort.java
+++ InsertionSort.java
@@ -13,9 +13,9 @@ public class InsertionSort {
         int temp;
 
         //comparing and sorting
-        for (int x = 1; x < array.length ; x++) { // loop for the length of array and swaps for the smallest
+        for (int x = 1; x < array.length ; x++) { // loop for the length of the avalaible array
             temp = array[x-1];
-           for (int y = x ; y < array.length ;y++) {
+           for (int y = x ; y < array.length ;y++) { //swaps "places" for the smallest number that it is remain of the array
                if (temp > array[y]){
                    temp = array[y];
                    array[y] = array[x-1];
