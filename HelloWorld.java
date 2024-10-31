defd5cbf60fcd59d6d4a095b73ac6c13212f5948
Nikola L <nikollauer@gmail.com>
2024-10-31 11:32:02 +0100

    Insertionsort and HelloWorld update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,11 @@
+/*
+ * @author Nikola Lauer
+ * @version 1.10 31 Oct 2024
+ *
+ * Test Project nr2 from first assigment for Course
+ *
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello"+args[0]);
--- InsertionSort.java
+++ InsertionSort.java
@@ -1,22 +1,28 @@
+/*
+ * @author Nikola Lauer
+ * @version 1.10 31 Oct 2024
+ *
+ * Test Project Sorting algorytm nr3 from first assigment for Course
+ * Insertion Sort
+ */
+
 public class InsertionSort {
     public static void main(String[] args) {
-        //Insertion Sort
         //initial variable
-        int[] array = {4, 15, 23, 3, 1, 2, 7, 19, 2, 6}; // for sorting
+        int[] array = {4, 15, 23, 3, 1, 2, 7, 19, 2, 6}; // array sample for sorting
         int temp;
-        //comparing and sorting
 
-        for (int x = 1; x < array.length ; x++) { // loop one minus the size of array
+        //comparing and sorting
+        for (int x = 1; x < array.length ; x++) { // loop for the length of array and swaps for the smallest
             temp = array[x-1];
-           for(int y = x ; y<array.length ;y++) {
-               if(temp > array[y]){
+           for (int y = x ; y < array.length ;y++) {
+               if (temp > array[y]){
                    temp = array[y];
-                   array[y] =array[x-1];
+                   array[y] = array[x-1];
                    array[x-1] = temp;
                }
 
            }
+        }
     }
-        System.out.print(array);
-}
 }
