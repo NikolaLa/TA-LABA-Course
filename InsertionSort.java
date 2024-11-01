d9f32166b1281810d4dfb0ed9825a30bd8a7c83c
Nikola L <nikollauer@gmail.com>
2024-11-01 16:29:48 +0100

    Insertion adding system output to show sorted array to consol

--- InsertionSort.java
+++ InsertionSort.java
@@ -13,16 +13,19 @@ public class InsertionSort {
         int temp;
 
         //comparing and sorting
-        for (int x = 1; x < array.length ; x++) { // loop for the length of the avalaible array
-            temp = array[x-1];
-           for (int y = x ; y < array.length ;y++) { //swaps "places" for the smallest number that it is remain of the array
-               if (temp > array[y]){
-                   temp = array[y];
-                   array[y] = array[x-1];
-                   array[x-1] = temp;
-               }
+        for (int x = 1; x < array.length; x++) { // loop for the length of the avalaible array
+            temp = array[x - 1];
+            for (int y = x; y < array.length; y++) { //swaps "places" for the smallest number that it is remain of the array
+                if (temp > array[y]) {
+                    temp = array[y];
+                    array[y] = array[x - 1];
+                    array[x - 1] = temp;
+                }
 
-           }
+            }
+        }
+        for (int x = 0; x < array.length; x++){ // printing output
+            System.out.print(array[x] + " ");
         }
     }
 }
