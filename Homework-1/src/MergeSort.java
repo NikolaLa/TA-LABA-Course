b06d86fc541eb9fb822f10db37bb2b1966c75d6d
Nikola L <nikollauer@gmail.com>
2024-11-03 00:33:14 +0100

    Updating naming and file rearage modules/folder

--- Homework-1/src/MergeSort.java
+++ Homework-1/src/MergeSort.java
@@ -10,22 +10,15 @@ public class MergeSort {
     public static void main(String[] args) {
         //initial variable
         int[] array = {4, 15, 23, 3, 1, 2, 7, 19, 2, 6}; // array sample for sorting
-        int temp;
 
-        //comparing and sorting
-        for (int x = 1; x < array.length; x++) { // loop for the length of the avalaible array
-            temp = array[x - 1];
-            for (int y = x; y < array.length; y++) { //swaps "places" for the smallest number that it is remain of the array
-                if (temp > array[y]) {
-                    temp = array[y];
-                    array[y] = array[x - 1];
-                    array[x - 1] = temp;
-                }
+        //Divide
+        //two equal parts
 
-            }
-        }
-        for (int x = 0; x < array.length; x++){ // printing output
-            System.out.print(array[x] + " ");
-        }
+        //Merge
+
+
+//        for (int x = 0; x < array.length; x++){ // printing output
+//            System.out.print(array[x] + " ");
+//        }
     }
 }
