75c14cf11b28f37166dec2c2cf158c3168a087f7
Nikola L <nikollauer@gmail.com>
2024-11-05 13:40:11 +0100

    Updating merge sort

--- Homework-1/src/MergeSort.java
+++ Homework-1/src/MergeSort.java
@@ -10,11 +10,27 @@ public class MergeSort {
     public static void main(String[] args) {
         //initial variable
         int[] array = {4, 15, 23, 3, 1, 2, 7, 19, 2, 6}; // array sample for sorting
+        int mid;
 
         //Divide
         //two equal parts
+        if(array.length %2 !=0) { // if even
+            mid = array.length / 2;
 
+        }
+        else {
+            mid = array.length / 2;
+
+        }
         //Merge
+        class MergeSort{
+            public void Divide(){
+
+            }
+            public void Merge(){
+
+            }
+        }
 
 
 //        for (int x = 0; x < array.length; x++){ // printing output
